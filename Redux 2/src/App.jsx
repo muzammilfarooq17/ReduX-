2ff5978f0df3a4d322680dc64129f2b1c37c5e47
muzammilfarooq17 <farooqmuzamil.127@gmail.com>
2026-06-15 03:40:54 +0500

    -

--- Redux 2/src/App.jsx
+++ Redux 2/src/App.jsx
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./redux/features/counterSlice"
 
 
 
 const App = () => {
+  const dispatch = useDispatch()
+  const count =  useSelector((state)=>state.counter.value)
   return (
     <div>
-      APP
+    <h1>{count}</h1>
+    <div>
+      <button onClick={()=>{
+        dispatch(increment())
+      }} className="p"  >Increment</button>
+      <button  onClick={()=>{
+        dispatch(decrement())
+      }} className="p" >Decrement </button>
+     
+    </div>
     </div>
   )
 }
